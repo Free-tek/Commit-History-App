4a6ffc525459622de1f099da0641ed622ac66942
Free-tek <adewole63@gmail.com>
2020-09-10 17:38:19 +0100

    view:tableview cell xib and UItableviewcell  class updated for each cell item

---
+++ Commit History/View/CommitTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommitTableViewCell.swift
+//  Commit History
+//
+//  Created by Botosoft Technologies on 10/09/2020.
+//  Copyright © 2020 mePlaylist. All rights reserved.
+//
+
+import UIKit
+
+class CommitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
