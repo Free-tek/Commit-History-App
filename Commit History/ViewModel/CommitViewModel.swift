417492e7b2506665120ecc019d24529138734131
Free-tek <adewole63@gmail.com>
2020-09-10 17:22:01 +0100

    view and viewmodel classes created for MVVM architecture

---
+++ Commit History/ViewModel/CommitViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CommitViewModel.swift
+//  Commit History
+//
+//  Created by Botosoft Technologies on 10/09/2020.
+//  Copyright © 2020 mePlaylist. All rights reserved.
+//
+
+import Foundation
